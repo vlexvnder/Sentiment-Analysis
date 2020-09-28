dc5ec3b4bc250cd5b538dcad3d160ae51deead77
Alex <vlexvnder@gmail.com>
2020-09-27 22:22:56 -0700

    debugged session variables which store past call to minimize Azure API calls

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, session, render_template, jsonify, request
 from Analyze import score as score_text
 from base64 import b64decode as b64d
+import requests as r
 app = Flask(__name__)
 
 app.secret_key = b'_5#y2gyhsghyttryrthgfjkjutrtqtregdfgdL"F4Q8z\n\asdasdasdas]/'
@@ -31,19 +32,19 @@ def finalScore():
     if request.method == "POST":
         text = request.form['content']
         if(text == session.get('text')):
-           return session.get('last_response')
-        subscription_key = "key"
-        endpoint = "endpoint"
+           return jsonify([session.get('last_response'), {'fromSessionVars':'true'}])
+        subscription_key = "<Key>"
+        endpoint = "<EndPoint>"
         sentiment_url = endpoint + "/text/analytics/v3.0/sentiment"
         documents = {"documents": [
         {"id": "1", "language": "en",
         "text": text},]}
         headers = {"Ocp-Apim-Subscription-Key": subscription_key}
-        response = requests.post(sentiment_url, headers=headers, json=documents)
+        response = r.post(sentiment_url, headers=headers, json=documents)
         sentiments = response.json()
         session['text'] = text
         session['last_response'] = sentiments
-        return sentiments
+        return jsonify(sentiments)
         
     return ""
 
