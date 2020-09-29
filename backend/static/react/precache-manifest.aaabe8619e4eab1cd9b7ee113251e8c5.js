7e5f0052e6cd06dd2d113338044b0ee27618b2cc
jamesg31 <jamie.gardner25@gmail.com>
2020-09-28 20:42:01 -0700

    Build

---
+++ backend/static/react/precache-manifest.aaabe8619e4eab1cd9b7ee113251e8c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7805549b68b2bbdc2628d57c0c63061a",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "7b336862d21630bf37e9",
+    "url": "/static/react/css/main.18af54cf.chunk.css"
+  },
+  {
+    "revision": "4bf6e4f2bb76bd669939",
+    "url": "/static/react/js/2.d651e180.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.d651e180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b336862d21630bf37e9",
+    "url": "/static/react/js/main.3a911255.chunk.js"
+  },
+  {
+    "revision": "292e3842b3df7ef43c03",
+    "url": "/static/react/js/runtime-main.0e709209.js"
+  }
+]);
